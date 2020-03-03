9d72e52f1ff4a0d7a800e1a2afb966318aa93ea9
BenMacD <benmdonagh@gmail.com>
2020-03-03 22:59:36 +0000

    Updates

---
+++ precache-manifest.044bac1981df8022e1305bd463421748.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9636d07fc7a44a8db7ed27d76b41e4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5ee2b70a7b1ae7fdeee0",
+    "url": "/portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "e26750b36fd237ffb903",
+    "url": "/portfolio/static/css/main.b304f4f4.chunk.css"
+  },
+  {
+    "revision": "5ee2b70a7b1ae7fdeee0",
+    "url": "/portfolio/static/js/2.5a8d36f6.chunk.js"
+  },
+  {
+    "revision": "668797747700face328339e70dedd258",
+    "url": "/portfolio/static/js/2.5a8d36f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e26750b36fd237ffb903",
+    "url": "/portfolio/static/js/main.3575a447.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "caccaeb3535591caee955cf477c7beac",
+    "url": "/portfolio/static/media/Ben.caccaeb3.png"
+  }
+]);
